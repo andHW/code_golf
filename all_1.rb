d3c7fbd205c394478416ed09f66caa6a3660b6ae
Andreas <and@reas.me>
2021-07-14 01:05:32 +0800

    add description

--- all_1.rb
+++ all_1.rb
@@ -1,1 +1,7 @@
+# 18 bytes
+# Input:    String that cotains an integer in decimal form
+# Output:   print true if the binary form of the input contains 1s only, otherwise it prints false.
+# Logic:    It prints true if the input does not contains zero <=> all bits are 1s.
+#           '%b'%gets converts integer string input to binary string magically
+#           ?0 instead of '0' to reduce 1 character magically
 p !('%b'%gets)[?0]
